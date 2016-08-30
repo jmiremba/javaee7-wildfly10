8f811bfb1b5697f75bc548a164065ea2ce2cf214
Jubilee Miremba <jmiremba@strive-ltd.com>
2016-08-30 05:36:07 -0600

    Hello World
    Initial setup in Eclipse with Wildfly runtime

---
+++ hello-world/src/com/strive/learn/javaee7/wildfly/servlet/FirstServlet.java
@@ -0,0 +1,41 @@
+package com.strive.learn.javaee7.wildfly.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class FirstServlet
+ */
+@WebServlet("/first-servlet")
+public class FirstServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	
+	private static final String CONTENT_TYPE = "text/html;charset=UTF-8";
+	private static final String MESSAGE = "<!DOCTYPE html><html>"+
+			"<head><title>Hello!</title></head>" +
+			"<body>Hello World WildFly</body>" +
+			"</html>";
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public FirstServlet() {
+        super();
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType(CONTENT_TYPE);
+		try (PrintWriter out = response.getWriter()) {
+			out.println(MESSAGE);
+		}
+	}
+}
